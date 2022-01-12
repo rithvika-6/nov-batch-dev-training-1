8f5cd9b5413d4f04f2b9dcae3d511c85ba169fda
Rithvika-6 <rithvika.sr@hcl.com>
2022-01-13 01:37:14 +0530

    LWC Code-javascript

---
+++ force-app/main/default/triggers/javascript/help.js
@@ -0,0 +1,14 @@
+function showmsg()
+{
+    alert('your ready to go');
+
+    // value to be read n printed
+    alert(document.getElementById('Name').value);
+
+}
+
+function printvalue()
+{
+    alert('key press is on')
+}
+
